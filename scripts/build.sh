d5ec9988479bc3adbc49305deb42f8e330c1ac8d
Jordan Klassen <jordan@klassen.me.uk>
2015-09-24 12:38:08 -0700

    finish test cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 set -e
 
 for f in packages/*; do
+  if [ -n "$PACKAGE" ] && [ packages/"$PACKAGE" != $f ] ; then continue; fi
   if [ -d "$f/src" ]; then
     node node_modules/babel/bin/babel "$f/src" --out-dir "$f/lib" --copy-files $1 &
   fi
